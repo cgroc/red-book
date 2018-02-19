b8d5dcd5b435b0866ad2d0844808b6651276a62a
Chris Grocott <chris.grocott@gmail.com>
2018-02-19 22:48:16 +0000

    Tail recursive implementation of fib

--- src/main/scala/ch02/ExercisesCh02.scala
+++ src/main/scala/ch02/ExercisesCh02.scala
@@ -5,12 +5,21 @@ import scala.annotation.tailrec
 object ExercisesCh02 {
 
   def factorial(i: Int): Int = {
-
     @tailrec
     def go(j: Int, acc: Int): Int =
       if(j <= 0) acc
       else go(j - 1, acc * j)
 
     go(i, 1)
+  }
+
+  def fib(i: Int): Int = {
+    @tailrec
+    def go(j: Int, finalAcc: Int, tempAcc: Int): Int = {
+      if (j < 1) finalAcc
+      else go(j - 1, tempAcc, finalAcc + tempAcc)
+    }
+
+    go(i, 0, 1)
   }
 }
--- src/test/scala/ch02/ExercisesCh02Spec.scala
+++ src/test/scala/ch02/ExercisesCh02Spec.scala
@@ -12,4 +12,23 @@ class ExercisesCh02Spec extends FlatSpec with Matchers {
     ExercisesCh02.factorial(0) shouldBe 1
   }
 
+  "Fibonacci" should "return 0 for non natural numbers" in {
+    ExercisesCh02.fib(0) shouldBe 0
+    ExercisesCh02.fib(-1) shouldBe 0
+  }
+
+  it should "return the correct value for the first three Fibbonacci numbers" in {
+    ExercisesCh02.fib(1) shouldBe 1
+    ExercisesCh02.fib(2) shouldBe 1
+    ExercisesCh02.fib(3) shouldBe 2
+  }
+
+  it should "return the correct value for the 20th Fibonacci number" in {
+    ExercisesCh02.fib(20) shouldBe 6765
+  }
+
+  it should "return 0 when called with numbers less than 1" in {
+    ExercisesCh02.fib(0) shouldBe 0
+    ExercisesCh02.fib(-23) shouldBe 0
+  }
 }
