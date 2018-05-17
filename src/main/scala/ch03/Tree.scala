51b6effe7a6b297edcf3a7deb4192ff97b17f96d
Chris Grocott <chris.grocott@gmail.com>
2018-05-17 12:53:23 +0100

    Feverishly finish off chapter 3 exercises

--- src/main/scala/ch03/Tree.scala
+++ src/main/scala/ch03/Tree.scala
@@ -19,16 +19,46 @@ object Tree {
   // 3.26
   // Write a function maximum that returns the maximum element in a Tree[Int]. (Note: In Scala, you can use x.max(y) or
   // x max y to compute the maximum of two integers x andy.)
+  def maximum(t: Tree[Int]): Int =
+    t match {
+      case Leaf(i) => i
+      case Branch(l, r) =>
+        val (maxl, maxr): (Int, Int) = (maximum(l), maximum(r))
+        if(maxl > maxr) maxl else maxr
+    }
 
   // 3.27
   // Write a function depth that returns the maximum path length from the root of a tree to any leaf.
+  def depth[A](tree: Tree[A]): Int = {
+    def loop(t: Tree[A], acc: Int): Tree[Int] =
+      t match {
+        case Leaf(_) => Leaf(acc)
+        case Branch(l, r) => Branch(loop(l, acc + 1), loop(r, acc + 1))
+      }
+    maximum(loop(tree, 0))
+  }
 
   // 3.28
-  // Write a function map, analogous to the method of the same name on List, that modi- fies each element in a tree with
+  // Write a function map, analogous to the method of the same name on List, that modifies each element in a tree with
   // a given function.
+  def map[A, B](tree: Tree[A])(f: A => B): Tree[B] =
+    tree match {
+      case Leaf(value) => Leaf(f(value))
+      case Branch(l, r) => Branch(map(l)(f), map(r)(f))
+    }
 
   // 3.29
   // Generalize size, maximum, depth, and map, writing a new function fold that abstracts over their similarities.
   // Reimplement them in terms of this more general function. Can you draw an analogy between this fold function and the
   // left and right folds for List?
+  def fold[A, B](tree: Tree[A], unit: B)(combine: (A, B) => B): B = {
+    def loop(t: Tree[A], acc: B): B =
+      t match {
+        case Leaf(a) => combine(a, acc)
+        case Branch(l, r) =>
+          val newacc: B = loop(l, acc)
+          loop(r, newacc)
+      }
+    loop(tree, unit)
+  }
 }
--- src/test/scala/ch03/TreeSpec.scala
+++ src/test/scala/ch03/TreeSpec.scala
@@ -17,4 +17,66 @@ class TreeSpec extends WordSpec with Matchers {
     }
   }
 
+  "Tree.maximum" should {
+
+    "return the value of a leaf" in {
+
+      val t = Leaf(7)
+      Tree.maximum(t) shouldBe 7
+    }
+
+    "return the highest value for a more complicated tree" in {
+
+      val t = Branch(Leaf(8), Leaf(20))
+      Tree.maximum(t) shouldBe 20
+    }
+  }
+
+  "Tree.depth" should {
+
+    "return 0 for a single leaf" in {
+
+      val t = Leaf(7)
+      Tree.depth(t) shouldBe 0
+    }
+
+    "return 2 for a tree with maximum depth 2" in {
+
+      val t = Branch(Leaf(7), Branch(Leaf(8), Leaf(20)))
+      Tree.depth(t) shouldBe 2
+    }
+  }
+
+  "Tree.map" should {
+
+    "multiply a tree of ints by 2" in {
+
+      val t = Branch(Leaf(7), Branch(Leaf(8), Leaf(20)))
+      val expected = Branch(Leaf(14), Branch(Leaf(16), Leaf(40)))
+      Tree.map(t)(_*2) shouldBe expected
+    }
+
+    "return a tree of string lengths" in {
+
+      val t = Branch(Leaf("One"), Branch(Leaf("Two"), Leaf("Three")))
+      val expected = Branch(Leaf(3), Branch(Leaf(3), Leaf(5)))
+      Tree.map(t)(_.length) shouldBe expected
+    }
+  }
+
+  "Tree.fold" should {
+
+    "sum all the elements in a tree" in {
+
+      val t = Branch(Leaf(7), Branch(Leaf(8), Leaf(20)))
+      Tree.fold[Int, Int](t, 0)(_+_) shouldBe 35
+    }
+
+    "concatenate all the strings in a tree" in {
+
+      val t = Branch(Leaf("One"), Branch(Leaf("Two"), Leaf("Three")))
+      Tree.fold[String, String](t, "")(_+_) shouldBe "ThreeTwoOne"
+    }
+  }
+
 }
